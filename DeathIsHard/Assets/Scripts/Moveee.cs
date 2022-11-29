d29190733dd5fc0927d245a0546ba6722658d20e
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2022-11-28 19:36:25 -0500

    left and right controls

---
+++ DeathIsHard/Assets/Scripts/Moveee.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveee : MonoBehaviour
+{
+    
+    Rigidbody2D rb;
+    public float sped = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * sped);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * sped);
+        }
+    }
+}
