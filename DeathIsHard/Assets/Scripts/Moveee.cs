dd8ed9dffc04fe19bd4aec39e3591c3da9ea6419
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2022-12-03 16:04:24 -0500

    Jumpee

--- DeathIsHard/Assets/Scripts/Moveee.cs
+++ DeathIsHard/Assets/Scripts/Moveee.cs
@@ -6,8 +6,12 @@ public class Moveee : MonoBehaviour
 {
     
     Rigidbody2D rb;
+    public Collider2D boxCollider2d;
+    public LayerMask platformLayerMask;
     public float sped = 40;
+    public float jumpHeight = 20f;
     public float bodySize = 0.72f;
+    public Animator animatr;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +24,46 @@ public class Moveee : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
+            animatr.SetBool("IsRunning", true);
             rb.AddForce(transform.right * sped);
             transform.localScale = new Vector2(bodySize, bodySize);
         }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animatr.SetBool("IsRunning", false);
+        }
         if (Input.GetKey(KeyCode.A))
         {
+            animatr.SetBool("IsRunning", true);
             rb.AddForce(-transform.right * sped);
             transform.localScale = new Vector2(-bodySize, bodySize);
         }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animatr.SetBool("IsRunning", false);
+        }
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+            float jumpVelocity = 10f;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    public bool IsGrounded() {
+        //return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded;
+
+        float extraHeightText = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, extraHeightText, platformLayerMask);
+
+        Color rayColor;
+        if (raycastHit.collider != null) {
+            rayColor = Color.green;
+        } else {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y + extraHeightText), Vector2.right * (boxCollider2d.bounds.extents.x * 2f), rayColor);
+
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
     }
 }
